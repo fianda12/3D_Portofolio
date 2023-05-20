e0cdd79c942d6a0309c65219e6f0bbd70e860edd
fikri amanda <fikriamd12@gmail.com>
2023-05-19 16:02:56 -1200

    change font

--- src/pages/page/Contact.tsx
+++ src/pages/page/Contact.tsx
@@ -67,7 +67,7 @@ const Contact = () => {
         className='flex-[0.75] bg-black-100 p-8 rounded-2xl mt-14'
       >
         <p className="sm:text-[18px] text-[14px] text-secondary uppercase tracking-wider">Get in touch</p>
-        <h3 className= "text-white font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]">Contact.</h3>
+        <h3 className= "text-white font-mono font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]">Contact.</h3>
 
         <form
           ref={formRef}
@@ -75,42 +75,42 @@ const Contact = () => {
           className='mt-12 flex flex-col gap-8'
         >
           <label className='flex flex-col'>
-            <span className='text-white font-medium mb-4'>Your Name</span>
+            <span className='text-white font-mono font-medium mb-4'>Your Name</span>
             <input
               type='text'
               name='name'
               value={form.name}
               onChange={handleChange}
               placeholder="What's your good name?"
-              className='bg-tertiary py-4 px-6 text-white rounded-lg outline-none border-none font-medium'
+              className='bg-tertiary py-4 px-6 font-mono text-white rounded-lg outline-none border-none font-medium'
             />
           </label>
           <label className='flex flex-col'>
-            <span className='text-white font-medium mb-4'>Your email</span>
+            <span className='text-white font-mono font-medium mb-4'>Your email</span>
             <input
               type='email'
               name='email'
               value={form.email}
               onChange={handleChange}
               placeholder="What's your web address?"
-              className='bg-tertiary py-4 px-6 text-white rounded-lg outline-none border-none font-medium'
+              className='bg-tertiary py-4 px-6 font-mono text-white rounded-lg outline-none border-none font-medium'
             />
           </label>
           <label className='flex flex-col'>
-            <span className='text-white font-medium mb-4'>Your Message</span>
+            <span className='text-white font-mono font-medium mb-4'>Your Message</span>
             <textarea
               rows={7}
               name='message'
               value={form.message}
               onChange={handleChange}
               placeholder='What you want to say?'
-              className='bg-tertiary py-4 px-6 text-white rounded-lg outline-none border-none font-medium'
+              className='bg-tertiary py-4 px-6 font-mono text-white rounded-lg outline-none border-none font-medium'
             />
           </label>
 
           <button
             type='submit'
-            className='bg-tertiary py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary'
+            className='bg-tertiary font-mono py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary'
           >
             {loading ? "Sending..." : "Send"}
           </button>
